5485e436f1fe0f6ed981daa1918f6b52d528e1f4
zhongethan <1661996960@qq.com>
2020-09-23 19:15:32 +0800

    add read-timing and write-timing pics

--- src/main/scala/asyncfifo/AsyncFifo.scala
+++ src/main/scala/asyncfifo/AsyncFifo.scala
@@ -1,67 +1,120 @@
 package asyncfifo
-import chisel3._
 
-class AsyncFifo (width : Int, depth : Int) extends RawModule {
+import chisel3._
+import chisel3.experimental.chiselName
+
+@chiselName
+class AsyncFifoB(width:Int,depth:Int) extends RawModule{
+  require(depth>1)
+
   val io = IO(new Bundle() {
     // write clock domain
     val wr_clk = Input(Clock())
     val wr_nrst = Input(AsyncReset())
-    var wr_en = Input(Bool())
+    val wr_en = Input(Bool())
     val wdata = Input(UInt(width.W))
 
     // read clock domain
     val rd_clk = Input(Clock())
     val rd_nrst = Input(AsyncReset())
-    var rd_en = Input(Bool())
+    val rd_en = Input(Bool())
     val rdata = Output(UInt(width.W))
     val empty = Output(UInt(1.W))
     val full = Output(UInt(1.W))
+
   })
+
   // regs num = 2 ^ depth
   val regs = SyncReadMem(1 << depth, UInt(width.W))
-  val wr_ptr_gray = Wire(UInt((depth+1).W))
-  val rd_ptr_gray = Wire(UInt((depth+1).W))
 
+  // these signals will be send from one clock domain to another
+  val wr_ptr_bin_next = Wire(UInt((depth+1).W))
+  val wr_ptr_gray_next = Wire(UInt((depth+1).W))
+  val rd_ptr_bin_next = Wire(UInt((depth+1).W))
+  val rd_ptr_gray_next = Wire(UInt((depth+1).W))
+
+  /**
+    * wr_clk domain
+    * 1. generate write pointer.
+    * 2. convert binary pointer to gray code
+    * 3. generate fifo full signal
+    */
   withClockAndReset(io.wr_clk,io.wr_nrst){
     // generate write pointer, plus 1 while write a new data
     val wr_ptr_bin = RegInit(0.U((depth+1).W))
-    val wr_ptr_bin_next = (io.wr_en && !io.full).asUInt() + wr_ptr_bin
+    wr_ptr_bin_next := (io.wr_en && !io.full).asUInt() + wr_ptr_bin
     wr_ptr_bin := wr_ptr_bin_next
-    val waddr = wr_ptr_bin(depth-1,0)
+
+    // convert binary code to gray code and register it
+    wr_ptr_gray_next := oneCycleDly(binToGray(wr_ptr_bin_next))
+
     // write data to regs
     when(io.wr_en && !io.full){
-      regs.write(waddr,io.wdata)
+      regs.write(wr_ptr_bin(depth-1,0),io.wdata)
     }
-    // binary to gray conversion
-    wr_ptr_gray := (wr_ptr_bin_next >> 1) ^ wr_ptr_bin_next
-    // sync rd_ptr_gray to wr_clk domain
-    val rd_ptr_r2w_d = RegNext(rd_ptr_gray)
-    val rd_ptr_r2w_dd = RegNext(rd_ptr_r2w_d)
     // fifo full signal
-    val rd_2nd_msb = rd_ptr_r2w_dd(depth) ^ rd_ptr_r2w_dd(depth-1)
-    val wr_2nd_msb = wr_ptr_gray (depth) ^ wr_ptr_gray(depth-1)
-    val f_full = RegInit(0.U(1.W))
-    f_full := ((wr_ptr_gray(depth)=/=rd_ptr_r2w_dd(depth))
-      && (rd_2nd_msb===wr_2nd_msb)
-      && (wr_ptr_gray(depth-2,0)===rd_ptr_r2w_dd(depth-2,0)))
-    io.full := f_full
+    io.full := isFifoFull(twoStageSync(rd_ptr_gray_next),binToGray(wr_ptr_bin_next))
   }
 
+  /**
+    * rd_clk domain
+    * 1. generate read pointer
+    * 2. convert binary pointer to gray code
+    * 3. generate fifo empty signal
+    */
   withClockAndReset(io.rd_clk,io.rd_nrst){
     // generate read pointer, plus 1 while read data out
     val rd_ptr_bin = RegInit(0.U((depth+1).W))
-    val rd_ptr_bin_next = (io.rd_en && !io.empty).asUInt() + rd_ptr_bin
+    rd_ptr_bin_next := (io.rd_en && !io.empty).asUInt() + rd_ptr_bin
     rd_ptr_bin := rd_ptr_bin_next
+
+    // convert binary code to gray code and register it
+    rd_ptr_gray_next := oneCycleDly(binToGray(rd_ptr_bin_next))
+
     // read data from regs
-    val raddr = rd_ptr_bin(depth-1,0)
-    io.rdata := regs.read(raddr,io.rd_en && !io.empty)
-    rd_ptr_gray := (rd_ptr_bin_next >> 1) ^ rd_ptr_bin_next
-    //sync wr_ptr_gray to rd_clk domain
-    val wr_ptr_w2r_d = RegNext(wr_ptr_gray)
-    val wr_ptr_w2r_dd = RegNext(wr_ptr_w2r_d)
+    io.rdata := regs.read(rd_ptr_bin(depth-1,0),io.rd_en && !io.empty)
     // fifo empty
-    val f_empty = RegInit(1.U(1.W))
-    f_empty := rd_ptr_gray === wr_ptr_w2r_dd
-    io.empty := f_empty
+    io.empty := isFifoEmpty(twoStageSync(wr_ptr_gray_next),binToGray(rd_ptr_bin_next))
+  }
+
+
+  private def twoStageSync(d:UInt):UInt = {
+    val stage_d  = RegInit(0.U(width.W))
+    val stage_dd = RegInit(0.U(width.W))
+    stage_d := d
+    stage_dd := stage_d
+    stage_dd
+  }
+
+  private def oneCycleDly(d:UInt):UInt = {
+    val dly = RegInit(0.U(width.W))
+    dly := d
+    dly
+  }
+
+  private def binToGray(d:UInt):UInt ={
+    (d>>1.U) ^ d
+  }
+
+  /**
+    * generate fifo full signal
+    * 0000-0010-0011-0010-0110-0111-0101-0100
+    * 1100-1110-1111-1110-1010-1011-1001-1000
+    * the top 2 bits should be opposite and the rest should be the same
+    */
+  private def isFifoFull(rd_ptr:UInt,wr_ptr:UInt):Bool={
+    val full = RegInit(0.U(1.W))
+    full := (rd_ptr(depth-2,0)===wr_ptr(depth-2,0)) && (rd_ptr(depth,depth-1) === (~(wr_ptr(depth,depth-1))).asUInt())
+    full.asBool()
+  }
+
+  /**
+    * generate fifo empty signal
+    * the read and write pointer should be totally the same
+    */
+  private def isFifoEmpty(rd_ptr:UInt,wr_ptr:UInt):Bool={
+    val empty = RegInit(1.U(1.W)) //reset value = 1
+    empty := rd_ptr === wr_ptr
+    empty.asBool()
   }
 }
